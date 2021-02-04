f27a339b59f62dc064cbefc9a3bbe50773d82504
Artis <artux28@hotmail.com>
2021-02-04 22:18:14 +0100

    end screen returns to main menu

---
+++ Sugarboy2.0/Assets/Scripts/ReturnBackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnBackToMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Return());
+    }
+
+    private IEnumerator Return()
+    {
+        yield return new WaitForSeconds(31f);
+        SceneManager.LoadScene(0);
+    }
+}
